171b6073777f0f469d5269cdb83dd5ce25896863
Alexander Hazankin <103335228+AlexanderHazankin@users.noreply.github.com>
2022-12-13 00:50:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ scissors = '''
 
 # Introduction
 player_name = str(input("Hello! I am your smart device. What is your name? \n"))
-player_name = player_name.title()
 print(f"Hallo {player_name},\n"
       "This is a rock, paper, scissors game. You play against me.\n"
       "We will play a \033[1m\033[34mseveral rounds Tournament \033[22m\033[39mand see who is stronger!!!")
@@ -49,44 +48,49 @@ rounds = input("How many rounds are you willing to play?\n")
 while not rounds.isdigit():
     print("Invalid choice. Please try again.")
     rounds = input("How many rounds are you willing to play?\n")
+
 # convert the input to an integer
-rounds = int(rounds)
-sleep(1)
+num_rounds = int(rounds)
+
 # Play the game for the specified number of rounds
-for user in range(rounds):
+for i in range(num_rounds):
 
     # User's input storing
-    user = input("What do you choose? Enter 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+    user_choice = input("What do you choose? Enter 0 for Rock, 1 for Paper or 2 for Scissors.\n")
     # Use a while loop to repeatedly prompt the user until they enter a valid choice
-    while not user.isdigit() or int(user) not in [0, 1, 2]:
+    while not user_choice.isdigit() or int(user_choice) not in [0, 1, 2]:
         print("Invalid choice. Please try again.")
-        user = input("Enter 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+        user_choice = input("Enter 0 for Rock, 1 for Paper or 2 for Scissors.\n")
     # convert the input to an integer
-    user = int(user)
-    if user == 0:
+    user_choice = int(user_choice)
+    if user_choice == 0:
         print(rock)
-    elif user == 1:
+    elif user_choice == 1:
         print(paper)
-    elif user == 2:
+    elif user_choice == 2:
         print(scissors)
 
 # Random selection generator for computer
-    pc = random.choice([0, 1, 2])
-    if pc == 0:
+    pc_choice = random.choice([0, 1, 2])
+    if pc_choice == 0:
         print(rock)
-    elif pc == 1:
+    elif pc_choice == 1:
         print(paper)
-    elif pc == 2:
+    elif pc_choice == 2:
         print(scissors)
 
 # Comparing user and computer choices to determine a winner (or a draw)
-    if user == 0 and pc == 2 or user == 1 and pc == 0 or user == 2 and pc == 1:
+    if user_choice == 0 and pc_choice == 2\
+            or user_choice == 1 and pc_choice == 0\
+            or user_choice == 2 and pc_choice == 1:
         print(f"{player_name}, you won this round!!!")
         user_winn += 1
-    elif pc == 0 and user == 2 or pc == 1 and user == 0 or pc == 2 and user == 1:
+    elif pc_choice == 0 and user_choice == 2\
+            or pc_choice == 1 and user_choice == 0\
+            or pc_choice == 2 and user_choice == 1:
         print(f"{player_name}, you lost this round!!!")
         pc_winn += 1
-    elif user == pc:
+    elif user_choice == pc_choice:
         print("It's a Draw.")
         draw += 1
 
